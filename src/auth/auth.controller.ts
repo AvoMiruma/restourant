ffcc9c11bc07a82f0753d101763a976a447693d0
Oleg Gubenko <gubenkooleg3@gmail.com>
2024-03-21 12:26:17 +0200

    modified:   src/auth/auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,9 @@ import { AuthenticateDto } from './dto/authenticate.dto';
 import { RegistrationDto } from './dto/registration.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+/**
+ * lol
+ */
 @ApiTags('Auth-Controller')
 @Controller('auth')
 export class AuthController {
